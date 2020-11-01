84a78b0acb86cf1f0fae66abd6d449f9bddf7e93
Bruce Berrios <bberr022@fiu.edu>
2020-11-01 18:17:22 -0500

    Test

--- main.c
+++ main.c
@@ -9,8 +9,8 @@ int main() {
     process_list[0] = create_process(0, 100, 0, 5);
     process_list[1] = create_process(1, 100, 1, 6);
     process_list[2] = create_process(2, 100, 2, 7);
-    process_list[3] = create_process(3, 100, 3, 5);
-    process_list[4] = create_process(4, 1000, 4, 9);
-    perf_data* pfd = first_fit(process_list, size, 1000);
+    process_list[3] = create_process(3, 1000, 3, 5);
+    process_list[4] = create_process(4, 1100, 4, 9);
+    perf_data* pfd = first_fit(process_list, size, 1100);
     printf("Failed: %d, Iterations: %d\n", pfd->failed_allocations, pfd->iter_steps);
 }
