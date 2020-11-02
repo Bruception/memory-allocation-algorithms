1e15a4e5938e2721c07e4659f823d7b8bf8f58c0
Bruce Berrios <bberr022@fiu.edu>
2020-11-01 19:36:38 -0500

    Test best fit

--- main.c
+++ main.c
@@ -7,11 +7,11 @@ int main() {
     int size = 5;
     process** process_list = (process**)(malloc(sizeof(process) * size));
     process_list[0] = create_process(0, 33, 0, 5);
-    process_list[1] = create_process(1, 75, 1, 6);
-    process_list[2] = create_process(2, 91, 2, 7);
-    process_list[3] = create_process(3, 1089, 3, 5);
-    process_list[4] = create_process(4, 238, 4, 9);
-    perf_data* pfd = first_fit(process_list, size, 1100);
+    process_list[1] = create_process(1, 75, 0, 6);
+    process_list[2] = create_process(2, 91, 0, 7);
+    process_list[3] = create_process(3, 1089, 0, 5);
+    process_list[4] = create_process(4, 238, 0, 9);
+    perf_data* pfd = best_fit(process_list, size, 1100);
     printf("--------------------------------\n");
     // Average verified with: ./main.out | grep "^External" | cut -d' ' -f3 | sed -e 's/%//g' | awk '{ ++total; sum += $1 } END { print "Average:", sum/total }'
     printf("Results:\n\tFailed Allocations: %d\n\tIterations: %d\n\tAverage External Fragmentation: %.2lf%%\n", pfd->failed_allocations, pfd->iterations, pfd->average_external_frag);
